841dab999b0d3a9754031fb45dc90b8406ba856d
Vitaly Zeenko <dev.zeenko@gmail.com>
2024-02-27 17:58:57 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import "./App.css";
+
+import SearchIcon from "./search.svg";
+import MovieCard from "./components/MovieCard";
+import Spinner from "./components/spinner/Spinner";
+
+// c032e2d7
+//c032e2d7-1e3e-4e3a-8f9e-3f3b7d3e3e3e
+
+const API_URL = "http://www.omdbapi.com/?apikey=c032e2d7";
+
+const App = () => {
+	const [isLoading, setIsLoading] = useState(false);
+	const [movies, setMovies] = useState([]);
+	const [search, setSearch] = useState("");
+	const searchMovies = async (title) => {
+		setIsLoading(true);
+		if (!title.length) {
+			setIsLoading(false);
+			return;
+		}
+
+		const response = await fetch(`${API_URL}&s=${title}`);
+		const data = await response.json();
+		setMovies(data.Search);
+		setIsLoading(false);
+	};
+
+	const onSearchChange = (e) => {
+		e.preventDefault();
+		setSearch(e.target.value);
+	};
+
+	return (
+		<div className="app">
+			<h1>MovieMaster</h1>
+
+			<div className="search">
+				<input
+					placeholder="Search for movies"
+					value={search}
+					onChange={onSearchChange}
+				/>
+
+				<img
+					src={SearchIcon}
+					alt="Search"
+					onClick={() => {
+						searchMovies(search);
+					}}
+				/>
+			</div>
+
+			{isLoading ? (
+				<Spinner />
+			) : movies?.length > 0 ? (
+				<div className="container">
+					{movies.map((el) => (
+						<MovieCard movie={el} />
+					))}
+				</div>
+			) : (
+				<div className="empty">
+					<h2>No movies found</h2>
+				</div>
+			)}
+		</div>
+	);
+};
 
 export default App;
---
+++ src/components/spinner/Spinner.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Spinner.css"; // Make sure to create this CSS file
+
+const Spinner = () => (
+	<div className="spinner-container">
+		<div className="spinner"></div>
+	</div>
+);
+
+export default Spinner;
